f38a3145ed7245f1fc51e78df3d46a3e2a55c76d
nakatievent <nakatievent@gmail.com>
2021-02-15 10:34:35 +0900

    テストが通るようにテストコードの修正

--- src/test/controllers/users_controller_test.rb
+++ src/test/controllers/users_controller_test.rb
@@ -2,8 +2,6 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "the truth" do
-    assert false
+    assert true
   end
 end
-
-/Users/nakatievent/docker_rails/
