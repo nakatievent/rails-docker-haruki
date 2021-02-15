4127f248d745c4527ecb8fdd4c62f3b8724411d1
nakatievent <nakatievent@gmail.com>
2021-02-15 09:51:18 +0900

    .circleciディレクトリの移動

--- src/test/controllers/users_controller_test.rb
+++ src/test/controllers/users_controller_test.rb
@@ -2,6 +2,8 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "the truth" do
-    assert true
+    assert false
   end
 end
+
+/Users/nakatievent/docker_rails/
